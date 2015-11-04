797fe224cca5dbecfe7ea173e862cc12c668bf2e
rawtoast24 <anirudh24@gmail.com>
2015-11-04 17:13:31 -0500

    Second attempt for e2

--- exercise2.py
+++ exercise2.py
@@ -20,10 +20,6 @@ def find(input_string, substring, start, end):
     If there is a match, the function returns the starting index of the string as an integer. If not, it returns -1
 
     """
-    # Check if the end argument is greater than the start argument
-    if end <= start + 1:
-        return "Please enter an end argument that is greater than the start argument by at least one."
-
     # Convert the string and substring into lowercase
     input_string = input_string.lower()
     substring = substring.lower()
@@ -31,10 +27,6 @@ def find(input_string, substring, start, end):
     # Create initial variables
     result = 0
     i = start
-
-    # Check if the entered start and end arguments are less than the length of the string
-    if start > len(input_string) - 1 or end > len(input_string):
-        return "Please enter a start and end integer that is less than the length of the input string."
 
     # Check to see if the substring is within the spliced string
     while i < end:
@@ -45,7 +37,7 @@ def find(input_string, substring, start, end):
             i += 1
     return -1
 
-#print find("This is an ex-parrot", "parrot", 0, 20)
+#find("This is an ex-parrot", "parrot", 0, 20)
 
 def multi_find(input_string, substring, start, end):
     """
@@ -63,14 +55,6 @@ def multi_find(input_string, substring, start, end):
     substring = substring.lower()
     i = start
 
-# Check if the end argument is greater than the start argument
-    if end <= start + 1:
-        return "Please enter an end argument that is greater than the start argument by at least one."
-
-    # Check if the entered start and end arguments are less than the length of the string
-    if start > len(input_string) - 1 or end > len(input_string):
-        return "Please enter a start and end integer that is less than the length of the input string."
-
     while i < end:
         if input_string[i: i + len(substring)] == substring:
             result = result + str(i) + ","
@@ -81,5 +65,5 @@ def multi_find(input_string, substring, start, end):
     result = result[0:len(result)-1]
     return result
 
-#print multi_find("Ni! Ai! Ni! Ai!", "Ni", 0, 14)
+# multi_find("Ni! Ni! Ni! Ni!", "Ni", 0, 25)
 
